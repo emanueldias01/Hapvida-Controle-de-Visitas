e1d45e097336c63c82fdaeecd2f146af364f5ffb
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-04-23 15:10:48 -0300

    upload dos arquivos

---
+++ ControleDeVisitas/src/main/java/br/com/hapvida/controledevisitas/ControleDeVisitas/ControleDeVisitasApplication.java
@@ -0,0 +1,13 @@
+package br.com.hapvida.controledevisitas.ControleDeVisitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeVisitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeVisitasApplication.class, args);
+	}
+
+}
---
+++ ControleDeVisitas/src/test/java/br/com/hapvida/controledevisitas/ControleDeVisitas/ControleDeVisitasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hapvida.controledevisitas.ControleDeVisitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeVisitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
