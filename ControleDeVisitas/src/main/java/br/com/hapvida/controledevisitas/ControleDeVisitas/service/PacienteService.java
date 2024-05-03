2086e727b51166828208f15f8acb0bae6ba4118a
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-05-03 11:04:14 -0300

    refatoracao dos metodos de validacao de paciente

--- ControleDeVisitas/src/main/java/br/com/hapvida/controledevisitas/ControleDeVisitas/service/PacienteService.java
+++ ControleDeVisitas/src/main/java/br/com/hapvida/controledevisitas/ControleDeVisitas/service/PacienteService.java
@@ -15,46 +15,10 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class PacienteService {
+public class PacienteService extends ValidacaoPaciente {
 
     @Autowired
     PacienteRepository pacienteRepository;
-
-    //metodos de validacao
-    private boolean validaPaciente(PacienteRequestDTO data) {
-
-        boolean podeCriarPaciente = false;
-        var nome = pacienteRepository.findByNome(data.nome());
-        var cpf = pacienteRepository.findByCpf(data.cpf());
-        var leito = pacienteRepository.findByNumeroLeito(data.numeroLeito());
-
-
-        //VALIDACOES DE DUPLICACOES
-        if(nome.isEmpty()){
-            if(cpf.isEmpty()){
-                if(leito.isEmpty()){
-                    podeCriarPaciente = true;
-                }else {
-                    throw new LeitoIndisponivelException("leito indisponivel");
-                }
-            }else{
-                throw new CpfJaCadastradoException("cpf ja cadastrado");
-            }
-        }else {
-            throw new PacienteJaCadastradoException("nome ja cadastrado");
-        }
-
-
-        //valida cpf
-        if(data.cpf().length() == 11){
-
-        }else {
-            podeCriarPaciente = false;
-        }
-
-        return podeCriarPaciente;
-    }
-
 
     //metodos service
 
